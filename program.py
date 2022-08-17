60e52ac5e52ea576170211044a2ef7b3af5bcec4
Zek Brown <zek-brown@live.nmit.ac.nz>
2022-08-17 23:00:59 +1200

    added key

--- program.py
+++ program.py
@@ -4,9 +4,9 @@ import os.path
 
 # -- Layouts that will be displayed --
 login_layout = [
-    [sg.Text("Username"), sg.InputText()],
-    [sg.Text("Password"), sg.InputText(password_char="*", key="-PASSWORD-")],
-    [sg.Button("Login"), sg.Button("Cancel")],
+    [sg.Text("Username"), sg.InputText(key='-USERNAME-')],
+    [sg.Text("Password"), sg.InputText(password_char="*", key='-PASSWORD-')],
+    [sg.Button("Login"), sg.Button("Close")],
 ]
 
 home_layout = [
@@ -29,17 +29,17 @@ layout = [
     [sg.Text('Login', key='-TITLE-')],
     [sg.Button('Home', key='-HOMEBUTTON-', visible=False), sg.Button('Data', key='-DATABUTTON-', visible=False), sg.Button('Graph', key='-GRAPHBUTTON-', visible=False)],
     [sg.Column(login_layout, key='-COL1-'), sg.Column(home_layout, visible=False, key='-COL2-'), sg.Column(data_layout, visible=False, key='-COL3-'), sg.Column(graph_layout, visible=False, key='-COL4-')],
-    [sg.Button('Close', visible=False, key='-CLOSE-')]
 ]
 
 
 window = sg.Window('Data + Graph Viewer', layout,)
 
-layout = 1  # The currently visible layout
+layout = 1  # The current visible layout
 while True:
     event, values = window.read()
     print(event, values)
     if event in (None, 'Close'):
+        print("Close")
         break
 
     if event == '-HOMEBUTTON-' or 'login':
@@ -50,17 +50,20 @@ while True:
         window['-HOMEBUTTON-'].update(visible=True)
         window['-DATABUTTON-'].update(visible=True)
         window['-GRAPHBUTTON-'].update(visible=True)
+        print("Home Page")
 
     if event == '-DATABUTTON-': 
         window[f'-COL{layout}-'].update(visible=False)
         layout = 3
         window[f'-COL{layout}-'].update(visible=True)
         window['-TITLE-'].update('Data')
+        print("Data Page")
 
     if event == '-GRAPHBUTTON-': 
         window[f'-COL{layout}-'].update(visible=False)
         layout = 4
         window[f'-COL{layout}-'].update(visible=True)
         window['-TITLE-'].update('Graph')
+        print("Graph Page")
 
 window.close()
